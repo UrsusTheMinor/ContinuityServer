11860255d6353e83b31483d77a8a315c4a90d0e7
UrsusTheMinor <reini.behr@gmail.com>
2026-02-16 10:30:45 +0100

    server old utp

--- ContinuityServer.Voice/obj/Debug/net8.0/ContinuityServer.Voice.AssemblyInfo.cs
+++ ContinuityServer.Voice/obj/Debug/net8.0/ContinuityServer.Voice.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ContinuityServer.Voice")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+152016030d465bb780dd102294fccdb95334221c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b5461b6a26ec703e789c4ecf4f991207bf85a69e")]
 [assembly: System.Reflection.AssemblyProductAttribute("ContinuityServer.Voice")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ContinuityServer.Voice")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
